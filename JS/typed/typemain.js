01b1ef59d07d8d15a56cc8986b9eb993bb3b316d
unknown <ankitaryakissu2@gmail.com>
2018-05-01 00:05:34 +0530

    typedjs, multiplejs,slided, p tag styling

---
+++ JS/typed/typemain.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.bigautotype', {
+    stringsElement: '#typed-string',
+
+    typeSpeed: 80,
+    showCursor: false,
+    backSpeed: 1000,
+    fadeOut: true,
+    loop: false
+  });
+
+
